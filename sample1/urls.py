2c637967f8510b733b82de4efc046cd9539a28c7
ganeshkumarnew <reachganeshkumar@outlook.com>
2020-05-31 19:47:59 +0530

    ftl task

--- sample1/urls.py
+++ sample1/urls.py
@@ -15,13 +15,14 @@ Including another URLconf
 """
 from django.conf.urls import url,include
 from django.contrib import admin
-from rest_framework import routers
 from . import views
-
+from rest_framework import routers, serializers, viewsets
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', views.home, name='home'),
-    url(r'^api/', views.UserViewSet.as_view(), name='users'),
+    # url(r'^api/v1/', views.UserViewSet.as_view(), name='users'),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
+
